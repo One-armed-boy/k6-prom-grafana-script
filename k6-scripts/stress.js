997bde751b56f03b3a1d1f7bbdd231c12091252f
One-armed-boy <fanatic5500@gmail.com>
2024-04-02 17:13:03 +0900

    기본 부하테스트가 k6 테스트용 도메인을 가르키도록 수정

--- k6-scripts/stress.js
+++ k6-scripts/stress.js
@@ -1,10 +1,10 @@
-import http from "k6/http";
-import { sleep } from "k6";
+import http from 'k6/http';
+import { sleep } from 'k6';
 export const options = {
-  vus: 1000,
-  duration: "30s",
+  vus: 10,
+  duration: '30s',
 };
 export default function () {
-  http.get("http://host.docker.internal:4000/api/ticketings");
+  http.get('http://test.k6.io');
   sleep(1);
 }
